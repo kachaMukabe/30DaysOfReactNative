4c67fbe5d2614da897de0db6fed27dc0911ded37
Kacha Mukabe <kachamukabe@gmail.com>
2018-07-26 23:12:34 +0200

    Login functions working. TODO: add social media logins

--- Login/App.js
+++ Login/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, Alert } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
+import {
+  Form,
+  Input,
+  Item,
+  Container,
+  Content,
+  Button,
+  Text,
+  Icon,
+  Body,
+  Left,
+  Right
+} from 'native-base';
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  withNavigation
+} from 'react-navigation';
 import * as firebase from 'firebase';
 
 
@@ -15,7 +33,13 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export default class App extends React.Component {
+  render(){
+    return <RootStack />
+  }
+}
 
+
+class LoginScreen extends React.Component{
   constructor(props){
     super(props)
     this.state = {
@@ -28,6 +52,7 @@ export default class App extends React.Component {
     try {
       await firebase.auth().createUserWithEmailAndPassword(email, pass)
       Alert.alert('user created')
+      this.props.navigation.navigate('Register')
     } catch (error) {
       console.log(error.toString())
     }
@@ -37,6 +62,7 @@ export default class App extends React.Component {
     try {
       await firebase.auth().signInWithEmailAndPassword(email, pass)
       Alert.alert('logged in')
+      this.props.navigation.navigate('Home')
     } catch (error) {
       console.log(error.toString())
     }
@@ -46,26 +72,120 @@ export default class App extends React.Component {
     try {
       await firebase.auth().signOut()
       Alert.alert('signed out')
+      this.props.navigation.navigate('Login')
     } catch (error) {
       console.log(error.toString())
     }
   }
 
+  static navigationOptions = {
+      title: 'Login',
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{ this.state.email}</Text>
-        <TextInput placeholder="Username" onChangeText={(text)=>{this.setState({email: text})}} />
+      // <View style={styles.container}>
+      //   <Text>{ this.state.email}</Text>
+      //   <TextInput placeholder="Username" onChangeText={(text)=>{this.setState({email: text})}} />
         
-        <Text>{this.state.password}</Text>
-        <TextInput placeholder="Password" onChangeText={(text)=>{this.setState({password: text})}} />
-        <Button title="Sign Up" onPress={()=>this.register(this.state.email, this.state.password)} />
-        <Button title="Log in" onPress={()=>this.login(this.state.email, this.state.password)} />
-        <Button title="Sign Out" onPress={()=>this.logout()} />
+      //   <Text>{this.state.password}</Text>
+      //   <TextInput placeholder="Password" onChangeText={(text)=>{this.setState({password: text})}} />
+      //   <Button title="Sign Up" onPress={()=>this.register(this.state.email, this.state.password)} />
+      //   <Button title="Log in" onPress={()=>this.login(this.state.email, this.state.password)} />
+      //   <Button title="Sign Out" onPress={()=> this.props.navigation.navigate('Home')} />
+      // </View>
+      <Container >
+        {/* <Body> */}
+          <Content padder>
+            <Form>
+              <Item>
+                <Input placeholder="email" onChangeText={(text)=>{this.setState({email: text})}}/>
+              </Item>
+              <Item last>
+                <Input secureTextEntry={true} placeholder="password" onChangeText={(text)=>{this.setState({password: text})}}/>
+              </Item>
+              
+            </Form>
+            <Button block style={styles.button} onPress={()=>this.login(this.state.email, this.state.password)}>
+              <Text>Log In</Text>
+            </Button>
+            <Button block style={styles.button} onPress={()=>this.register(this.state.email, this.state.password)}>
+              <Text>Register</Text>
+            </Button>
+          </Content>
+          
+        {/* </Body> */}
+      </Container>
+    );
+  }
+}
+
+class RegisterScreen extends React.Component{
+  static navigationOptions = {
+    title: 'Registration',
+  }
+  render(){
+    return(
+      <View>
+        <Text>Register Screen</Text>
+      </View>
+    );
+    
+  }
+}
+
+class HomeScreen extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  async logout(){
+    try {
+      await firebase.auth().signOut()
+      console.log('signed out')
+      //Alert.alert('signed out')
+    } catch (error) {
+      console.log(error.toString())
+    }
+  }
+
+  componentDidMount(){
+    this.props.navigation.setParams({logout: ()=>{
+      //Alert.alert('aaaa');
+      this.logout();
+      this.props.navigation.navigate('Login')
+    }})
+  }
+
+  static navigationOptions = ({navigation}) => {
+    return{
+      title: 'Home',
+      headerRight: (
+        <Button transparent onPress={navigation.getParam('logout')}><Icon name="md-log-out"/></Button>
+      ),
+    }
+    
+  }
+  render(){
+    return(
+      <View>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
+
+const RootStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Register: RegisterScreen,
+    Home: HomeScreen
+  },
+  {
+    initialRouteName: 'Login',
+  }
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -73,5 +193,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    marginTop: 10,
   },
 });
