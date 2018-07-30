1ec9cb629577bda58d50dbd4c5b2b24634fe90db
Kacha Mukabe <kachamukabe@gmail.com>
2018-07-30 23:56:47 +0200

    Struggling with asyncstorage, will implement with firebase

--- Notes/App.js
+++ Notes/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Alert, AsyncStorage, Modal} from 'react-native';
+import store from 'react-native-simple-store';
 import {
   Container,
   Content,
@@ -22,10 +23,17 @@ export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      title: '',
-      note: '',
+      title: 'First',
+      note: 'Kkkkkkk',
+      notes: [],
+      newNote: '',
       modalVisible: false
     }
+    this._storeData()
+    store.update('album', {
+      albumName: 'Blurry Face'
+    })
+    
   }
 
   _storeData = async() => {
@@ -39,9 +47,10 @@ export default class App extends React.Component {
 
   _retrieveData = async(note) =>{
     try {
-      const value = await AsyncStorage.getItem(note)
+      const value = await AsyncStorage.getItem(note).then((text)=>{return text})
       if(value != null){
-        console.log('reached here')
+        //console.log(typeof(value))
+        const item = JSON.stringify(value)
         return value
       } else{
         return ''
@@ -51,11 +60,31 @@ export default class App extends React.Component {
     }
   }
 
+  getNote = (note) =>{
+    let no = '[]'
+    no = JSON.stringify(store.get('album')
+    .then((res) =>{
+      return res.albumName
+      }
+    ))
+    console.log(typeof(no))
+    
+    return no
+    // console.log(typeof(curNote))
+  }
+
   _getAllNotes = async() =>{
     try {
       const value = await AsyncStorage.getAllKeys()
+      this.setState({notes: value})
+      let notes = []
+      value.forEach((item, index)=>{
+        //console.log(item+" "+index)
+        notes.push(item)
+      })
+      //console.log(notes)
       if(value != null){
-        return value
+        return notes
       }
     } catch (error) {
       console.log(error.toString())
@@ -63,14 +92,14 @@ export default class App extends React.Component {
   }
 
   render() {
-    console.log(this._getAllNotes())
+    console.log(this.state.notes)
     return (
       <Container>
         <Header>
 
         </Header>
         <View style={{flex:1}}>
-          <List dataArray={['w', 'e']}
+          <List dataArray={this.state.notes}
             renderRow={(item)=>
               <ListItem>
                 <View style={{flex:1}}>
@@ -79,7 +108,7 @@ export default class App extends React.Component {
                   <Text>{item}</Text>
                 </CardItem>
                 <CardItem cardBody>
-                  {/* <Text>{this._retrieveData(item)}</Text> */}
+                  <Text>{this.getNote(item)}</Text>
                 </CardItem>
               </Card>
               </View>
@@ -87,7 +116,7 @@ export default class App extends React.Component {
             }
           />
           <Fab style={{ backgroundColor: '#5067FF' }}
-            onPress = {()=>{Alert.alert('pressed')}}
+            onPress = {()=>{this._getAllNotes()}}
             position="bottomRight">
             <Icon name="add" />
           </Fab>
